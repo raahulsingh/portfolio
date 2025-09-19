53e268592147fd3e5cae797aa9c1aa0d805b3b31
raahulsingh <96874428+raahulsingh@users.noreply.github.com>
2025-09-20 00:21:27 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import './Hero.css';
 
 const Hero = () => {
   return (
-    <header session id ="hero" className="hero-section">
+    <session id ="hero" className="hero-section">
       <div className="container hero-content">
         <h1 className="hero-name">Hello, I'm <br /><span>Rahul Kumar</span></h1>
         <p className="hero-tagline">
@@ -17,7 +17,7 @@ const Hero = () => {
           <a href="mailto:rahulkumarydv576@gmail.com" target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope"></i></a>
         </div>
       </div>
-    </header>
+    </session>
   );
 };
 
